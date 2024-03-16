8a6fb70f6e95fff4babf1b2d943fa3a14e58b9fa
Alina Lavrik <alina1voro@gmail.com>
2024-03-16 12:14:09 +0300

    Add Seminar 05

---
+++ DOM_Seminar/data.js
@@ -0,0 +1,26 @@
+const dataInfo = `[
+    {
+    "class": "news-container",
+    "url": "https://w.forfun.com/fetch/8e/8e0c065fe670d5b6c49de9f5fea277a4.jpeg",
+    "width": 765,
+    "heigt": 334,
+    "heading": "Lorem, ipsum dolor.",
+    "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam id minus earum."
+    },
+    {
+        "class": "news-container",
+        "url": "https://w.forfun.com/fetch/fd/fd5dd4e04b5a65807bac5c49ddd4c241.jpeg",
+        "width": 765,
+        "heigt": 1061,
+        "heading": "Lorem, ipsum dolor.",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam id minus earum."
+    },
+    {
+        "class": "news-container",
+        "url": "https://tipik.ru/wp-content/uploads/2019/06/%D0%9F%D1%83%D0%BC%D0%B0-%D1%81%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8003.jpg",
+        "width": 765,
+        "heigt": 1024,
+        "heading": "Lorem, ipsum dolor.",
+        "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam id minus earum."
+    }
+]`
