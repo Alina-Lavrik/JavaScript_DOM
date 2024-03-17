22aac840c866803bc2926bbb555aa03544ddbe7a
Alina Lavrik <alina1voro@gmail.com>
2024-03-17 19:51:02 +0300

    add seminar 06

---
+++ DOM_Seminar/data6.js
@@ -0,0 +1,36 @@
+const dataProducts = `
+[
+    {
+        "img": "https://p0.piqsels.com/preview/348/102/440/fashion-men-clothes-t-shirts.jpg",
+        "title": "MANGO  PEOPLE  T-SHIRT",
+        "price": "$300",
+        "color": "White",
+        "size": "Xl",
+        "quantity": "2"
+    },
+    {
+        "img": "https://p0.piqsels.com/preview/959/438/526/sneakers-jeans-scarf-blue.jpg",
+        "title": "ELLERY X M'O CAPSULE",
+        "price": "$520",
+        "color": "Green",
+        "size": "43",
+        "quantity": "1"
+    },
+    {
+        "img": "https://p0.piqsels.com/preview/669/668/848/grass-outdoor-people-girl-thumbnail.jpg",
+        "title": "MANGO  PEOPLE  TOP",
+        "price": "$100",
+        "color": "Green",
+        "size": "XS",
+        "quantity": "1"
+    },
+    {
+        "img": "https://p0.piqsels.com/preview/318/217/172/asian-girl-woman-people-thumbnail.jpg",
+        "title": "ELLERY DRESS CAPSULE",
+        "price": "$140",
+        "color": "Green",
+        "size": "S",
+        "quantity": "1"
+    }
+]`
+
