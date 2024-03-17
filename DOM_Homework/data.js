31ad542056e4a696126ddaad9e09ce9bd84118d0
Alina Lavrik <alina1voro@gmail.com>
2024-03-17 15:32:13 +0300

    add hometask 4

---
+++ DOM_Homework/data.js
@@ -0,0 +1,20 @@
+const birds = `[
+    {
+      "class": "bird",
+      "title": "Defense the travel audience hand",
+      "user": 28,
+      "id": 1,
+      "description": "Leader structure safe or black late wife newspaper her pick central forget single likely.",
+      "url": "https://api.slingacademy.com/public/sample-photos/1.jpeg"
+    },
+    {
+      "class": "bird",
+      "title": "Defense the travel audience hand",
+      "user": 28,
+      "id": 1,
+      "description": "Leader structure safe or black late wife newspaper her pick central forget single likely.",
+      "url": "https://api.slingacademy.com/public/sample-photos/1.jpeg"
+    }
+
+]
+`
